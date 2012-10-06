f5c6579b700e9bebeb1a11d4117322c93c458961
Charles Burgess <cvburgess@gmx.com>
2012-10-06 13:04:59 -0400

    Initial Comit

---
+++ USF Dining/USF Dining/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  USF Dining
+//
+//  Created by Charles Burgess on 10/6/12.
+//  Copyright (c) 2012 SquareOne Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ USF Dining/USF Dining/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  USF Dining
+//
+//  Created by Charles Burgess on 10/6/12.
+//  Copyright (c) 2012 SquareOne Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
